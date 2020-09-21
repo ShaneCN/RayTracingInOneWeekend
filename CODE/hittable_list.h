168cac3ae8b636b4995a851617502a6ccd13ff20
ShaneCN <49135402+ShaneCN@users.noreply.github.com>
2020-09-21 23:22:20 +0800

    6-2

--- CODE/hittable_list.h
+++ CODE/hittable_list.h
@@ -7,4 +7,42 @@
 
 using std::shared_ptr;
 using std::make_shared;
+class hittable_list:public hittable{
+    public:
+        hittable_list(){}
+        hittable_list(shared_ptr<hittable_list> object){add(object);}
+        
+        // 清空对象
+        void clear(){objects.clear();}
+        // 增长对象列表
+        void add(shared_ptr<hittable> object){objects.push_back(object);}
+        // 是否有碰撞
+        virtual bool hit(const ray& r,double t_min,double tmax,hit_record& rec) const;
+
+    public:
+        // 对象列表
+        std::vector<shared_ptr<hittable>> objects;
+};
+
+// 是否有碰撞 r为光线，t_min 为最小追踪时间，t_max 为最长追踪时间，rec是碰撞记录
+bool hittable_list::hit(const ray& r, double t_min, double t_max, hit_record& rec) const {
+    // 创建一个 hit记录
+    hit_record temp_rec;
+    // 记录是否碰撞
+    bool hit_anything = false;
+    auto closest_so_far = t_max;
+
+    // 遍历所有对象
+    for(const auto& object : objects){
+        // 如果有对象和这个光线发生碰撞
+        if(object->hit(r,t_min,closest_so_far,temp_rec)){
+            hit_anything = true;
+            closest_so_far = temp_rec.t;
+            rec = temp_rec;
+        }
+    }
+    return hit_anything;
+}
+
+#endif
 
--- CODE/vec3.h
+++ CODE/vec3.h
@@ -1,8 +1,11 @@
+#ifndef VEC3_H
+#define VEC3_H
+
+
 #include <iostream>
 #include <math.h>
 
-class vec3
-{
+class vec3 {
 public:
     vec3() : e{0, 0, 0} {}
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
@@ -98,3 +101,5 @@ inline vec3 cross(const vec3 &u, const vec3 &v) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
+
+#endif
